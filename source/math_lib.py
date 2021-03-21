dcda32f34958f481d07f92a7d17a6fb1bc26816c
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2021-03-21 23:02:48 +0100

    Added function of multiplication to math library

---
+++ source/math_lib.py
@@ -0,0 +1,26 @@
+# Functions for mathematical library
+
+##
+# @file math_lib.py
+# @brief Functions for mathematical library
+# @author Evgenii Shiliaev (xshili00)
+# @date 2021-03-21
+
+##
+# Function of adding two values
+#
+# @param a First addend
+# @param b Second addend
+# @return Return sum of two values
+def add(a, b):
+    return a + b
+
+
+##
+# Function of multiplying two values
+#
+# @param a First factor
+# @param b Second factor
+# @return Return product of two values
+def mul(a, b):
+    return a * b
