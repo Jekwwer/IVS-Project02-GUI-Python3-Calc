cdd3274604e00344e1478c583a62625e17a7f71a
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2021-03-22 22:01:09 +0100

    Added root function

--- source/math_lib.py
+++ source/math_lib.py
@@ -4,7 +4,6 @@
 # @file math_lib.py
 # @brief Functions for mathematical library
 # @author Evgenii Shiliaev (xshili00)
-# @date 2021-03-21
 
 ##
 # Function of adding two values
@@ -24,3 +23,17 @@ def add(a, b):
 # @return Return product of two values
 def mul(a, b):
     return a * b
+
+
+##
+# Function of general root two values
+#
+# @todo the function should be changed for x < 0 and n < 0 values
+#
+# @param x Radicand
+# @param n index of the radical
+# @return Return n-th root of radicand
+def root(x, n):
+    if x < 0 or n <= 0:
+        raise ValueError
+    return round(x ** (1 / n), 10)
