44b8497c69def5f769604e1574fa069d279baffd
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-06 11:54:41 +0200

    added function "diameter"
    + input numbers can be separated also by tabulator or end of line

--- source/profiling.py
+++ source/profiling.py
@@ -1,6 +1,15 @@
 import math_lib
 
+
+def diameter(list_numbers):
+    sum_numbers = 0
+    for number in list_numbers:
+        sum_numbers = math_lib.add(sum_numbers, int(number))
+    return sum_numbers / len(list_numbers)
+
+
 if __name__ == "__main__":
     string_input = input("Insert numbers for profiling (max 1000 numbers):")
-    num_list = string_input.split(" ")
-    print(num_list[0])
+    num_list = string_input.replace("\t", " ").replace("\n", " ").split(" ")
+    my_diam = diameter(num_list)
+    print(my_diam)
