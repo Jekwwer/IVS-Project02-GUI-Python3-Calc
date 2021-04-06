fe9ed07c8ad0a3918e94183bdaedfc2561c8758f
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-06 11:19:44 +0200

    added reading input

---
+++ source/profiling.py
@@ -0,0 +1,6 @@
+import math_lib
+
+if __name__ == "__main__":
+    string_input = input("Insert numbers for profiling (max 1000 numbers):")
+    num_list = string_input.split(" ")
+    print(num_list[0])
