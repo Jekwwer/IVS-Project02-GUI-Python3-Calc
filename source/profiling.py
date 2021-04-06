654adf21e2ba2833da9aa8688967345d239ddcbe
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-06 17:33:38 +0200

    added comments for doxygen

--- source/profiling.py
+++ source/profiling.py
@@ -1,6 +1,18 @@
+##
+# @file     profiling.py
+# @brief    Reads numbers and prints standard deviation
+# @author   Simon Brazda    (xbrazd22)
+# @date     2021-04-06
+
+
 import math_lib
 
 
+##
+# Function of sum of numbers in list
+#
+# @param list_numbers   List of numbers
+# @return               Returns sum of numbers from the list
 def sum_numbers(list_numbers):
     total = 0
     for number in list_numbers:
@@ -8,10 +20,20 @@ def sum_numbers(list_numbers):
     return total
 
 
+##
+# Function for diameter
+#
+# @param list_numbers   List of numbers
+# @return               Returns diameter of numbers form the list
 def diameter(list_numbers):
     return sum_numbers(list_numbers) / len(list_numbers)
 
 
+##
+# Function of standard deviation
+#
+# @param list_numbers   List of numbers
+# @return               Returns standard deviation
 def standard_deviation(list_numbers):
     a = 0
     for number in list_numbers:
