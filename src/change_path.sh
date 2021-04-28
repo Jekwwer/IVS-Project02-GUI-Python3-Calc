833f92f0a49fb226061c6c188feeba613bae980a
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-28 17:48:52 +0200

    change_path.sh was moved into install.sh

--- src/change_path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-new_path="/usr/share/WaterLift_calc/manual_for_help_window.pdf"
-input_file=$1
-
-RESULT=$(awk -v new_path="$new_path" '{gsub(/manual_for_help_window.pdf/,new_path)}1' "$input_file")
-echo "$RESULT" > "$input_file"
