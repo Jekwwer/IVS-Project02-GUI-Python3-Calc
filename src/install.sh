debe2c4d314d90f71b6581f9b66dc731927d2119
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-28 22:39:44 +0200

    corrected file name

--- src/install.sh
+++ src/install.sh
@@ -10,7 +10,7 @@ cp math_lib.py '../install/usr/share/waterlift-calc/math_lib.py'
 mkdir -p '../install/usr/share/pixmaps' 2>/dev/null
 cp ../images/WaterLift_Calculator_logo.png '../install/usr/share/pixmaps/waterlift_calc_logo.png'
 mkdir -p '../install/usr/bin' 2>/dev/null
-ln -sf /usr/share/waterlift_calc/gui.py '../install/usr/bin/waterlift-calc'
+ln -sf /usr/share/waterlift-calc/gui.py '../install/usr/bin/waterlift-calc'
 chmod 0775 '../install/DEBIAN/postinst'
 chmod 0775 '../install/DEBIAN/control'
 dpkg-deb --build '../install/' '../install/waterlift_calc-1.0-ubuntu20.04-x64.deb'
