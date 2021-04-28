2988cfcd059ab9f22fd054a5126ccf6cb72b78a6
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-28 23:03:37 +0200

    corrected path to logo

--- src/install.sh
+++ src/install.sh
@@ -8,7 +8,7 @@ cp gui.py '../install/usr/share/waterlift-calc/gui.py'
 cp manual_for_help_window.pdf '../install/usr/share/waterlift-calc/manual_for_help_window.pdf'
 cp math_lib.py '../install/usr/share/waterlift-calc/math_lib.py'
 mkdir -p '../install/usr/share/pixmaps' 2>/dev/null
-cp ../images/WaterLift_Calculator_logo.png '../install/usr/share/pixmaps/waterlift_calc_logo.png'
+cp ../images/waterlift_calc_logo.png '../install/usr/share/pixmaps/waterlift_calc_logo.png'
 mkdir -p '../install/usr/bin' 2>/dev/null
 ln -sf /usr/share/waterlift-calc/gui.py '../install/usr/bin/waterlift-calc'
 chmod 0775 '../install/DEBIAN/postinst'
