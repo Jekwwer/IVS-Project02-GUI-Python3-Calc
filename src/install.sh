35b7b098f143e80a4660ad8b88b4b7a3c1a2ddbf
simsa31 <79607496+simsa31@users.noreply.github.com>
2021-04-29 17:43:04 +0200

    make some files executable

--- src/install.sh
+++ src/install.sh
@@ -5,12 +5,15 @@ awk '{gsub("../images/waterlift_calc_logo.png","/usr/share/pixmaps/waterlift_cal
 
 mkdir -p '../install/usr/share/waterlift-calc' 2>/dev/null
 cp gui.py '../install/usr/share/waterlift-calc/gui.py'
+chmod +x '../install/usr/share/waterlift-calc/gui.py'
 cp manual_for_help_window.pdf '../install/usr/share/waterlift-calc/manual_for_help_window.pdf'
 cp math_lib.py '../install/usr/share/waterlift-calc/math_lib.py'
 mkdir -p '../install/usr/share/pixmaps' 2>/dev/null
 cp ../images/waterlift_calc_logo.png '../install/usr/share/pixmaps/waterlift_calc_logo.png'
+chmod +x '../install/usr/share/pixmaps/waterlift_calc_logo.png'
 mkdir -p '../install/usr/bin' 2>/dev/null
 ln -sf /usr/share/waterlift-calc/gui.py '../install/usr/bin/waterlift-calc'
 chmod 0775 '../install/DEBIAN/postinst'
 chmod 0775 '../install/DEBIAN/control'
+chmod +x '../install/usr/share/applications/waterlift_calc.desktop'
 dpkg-deb --build '../install/' '../install/waterlift_calc-1.0-ubuntu20.04-x64.deb'
